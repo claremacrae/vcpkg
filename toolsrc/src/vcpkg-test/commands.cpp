81b0793ed0fe2b5b1c6c25e914ff2406f0755cfd
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-07-24 14:06:07 +0100

    t   Add minimal test for Commands::get_available_commands_type_b()

--- toolsrc/src/vcpkg-test/commands.cpp
+++ toolsrc/src/vcpkg-test/commands.cpp
@@ -26,6 +26,29 @@ TEST_CASE ("get_available_commands", "[commands_get_available_commands]")
     };
 
     {
+        const auto& commands = Commands::get_available_commands_type_b();
+        CHECK(find_command(commands, "/?") != nullptr);
+        CHECK(find_command(commands, "help") != nullptr);
+        CHECK(find_command(commands, "search") != nullptr);
+        CHECK(find_command(commands, "list") != nullptr);
+        CHECK(find_command(commands, "integrate") != nullptr);
+        CHECK(find_command(commands, "owns") != nullptr);
+        CHECK(find_command(commands, "update") != nullptr);
+        CHECK(find_command(commands, "edit") != nullptr);
+        CHECK(find_command(commands, "create") != nullptr);
+        CHECK(find_command(commands, "cache") != nullptr);
+        CHECK(find_command(commands, "portsdiff") != nullptr);
+        CHECK(find_command(commands, "autocomplete") != nullptr);
+        CHECK(find_command(commands, "hash") != nullptr);
+        CHECK(find_command(commands, "fetch") != nullptr);
+        CHECK(find_command(commands, "x-ci-clean") != nullptr);
+        CHECK(find_command(commands, "x-history") != nullptr);
+        CHECK(find_command(commands, "x-vsinstances") != nullptr);
+        CHECK(find_command(commands, "x-format-manifest") != nullptr);
+        CHECK(find_command(commands, "wibble") == nullptr);
+    }
+
+    {
         const auto& commands = Commands::get_available_commands_type_c();
         CHECK(find_command(commands, "version") != nullptr);
         CHECK(find_command(commands, "contact") != nullptr);
