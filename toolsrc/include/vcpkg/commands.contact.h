707905feb46e6cc3a2fd9e828ecbae1ccb604e99
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-07-26 12:31:24 +0100

    r   Add ContactCommand

--- toolsrc/include/vcpkg/commands.contact.h
+++ toolsrc/include/vcpkg/commands.contact.h
@@ -9,5 +9,10 @@ namespace vcpkg::Commands
         extern const CommandStructure COMMAND_STRUCTURE;
         const std::string& email();
         void perform_and_exit(const VcpkgCmdArguments& args, Files::Filesystem& fs);
+
+        class ContactCommand : BasicCommand
+        {
+            void perform_and_exit(const VcpkgCmdArguments& args, Files::Filesystem& fs) const override;
+        };
     }
 }
--- toolsrc/src/vcpkg-test/commands.cpp
+++ toolsrc/src/vcpkg-test/commands.cpp
@@ -1,5 +1,6 @@
 #include <catch2/catch.hpp>
 
+#include <vcpkg/commands.contact.h>
 #include <vcpkg/commands.version.h>
 
 using namespace vcpkg;
@@ -7,5 +8,6 @@ using namespace vcpkg;
 TEST_CASE ("check all commands are constructable", "[commands]")
 {
     using namespace Commands;
+    Contact::ContactCommand c;
     Version::VersionCommand v;
 }
