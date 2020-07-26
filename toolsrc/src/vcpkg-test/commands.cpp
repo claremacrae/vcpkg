aaebdba5beb193792059241662a4a0336f4ebdb3
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-07-26 12:36:27 +0100

    t   Add tests of get_available_commands_type_c()

--- toolsrc/src/vcpkg-test/commands.cpp
+++ toolsrc/src/vcpkg-test/commands.cpp
@@ -1,6 +1,7 @@
 #include <catch2/catch.hpp>
 
 #include <vcpkg/commands.contact.h>
+#include <vcpkg/commands.h>
 #include <vcpkg/commands.version.h>
 
 using namespace vcpkg;
@@ -10,4 +11,14 @@ TEST_CASE ("check all commands are constructable", "[commands]")
     using namespace Commands;
     Contact::ContactCommand c;
     Version::VersionCommand v;
+}
+
+TEST_CASE ("check all command names are found", "[commands]")
+{
+    auto commands_raw = Commands::get_available_commands_type_c();
+    std::vector<Commands::PackageNameAndFunction<Commands::CommandTypeC>> commands(commands_raw.begin(),
+                                                                                   commands_raw.end());
+    CHECK(Commands::find("version", commands) != nullptr);
+    CHECK(Commands::find("contact", commands) != nullptr);
+    CHECK(Commands::find("wibble", commands) == nullptr);
 }
